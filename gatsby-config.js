03f2c54f0dbdc950f2a920e694b10e6d07b1ba50
Paul McCrodden <info@paulmccrodden.com>
2019-01-12 23:09:13 +0900

    Added dotenv lib and ENV var reference for deploy.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const languages = require('./src/data/languages');
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby with Contentful`,
+    title: `Ahimsa Yoga Center Website, powered by Gatsby & Contentful`,
     languages
   },
   plugins: [
@@ -18,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `rocybtov1ozk`,
-        accessToken: `6f35edf0db39085e9b9c19bd92943e4519c77e72c852d961968665f1324bfc94`,
+        spaceId: process.env.CONTENTFUL_SPACE_ID || '',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || ''
       },
     },
     {
