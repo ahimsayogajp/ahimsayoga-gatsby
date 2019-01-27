4726c04ce07704dc12db1f38226ff8ab81247a71
Paul McCrodden <info@paulmccrodden.com>
2019-01-27 12:58:50 +0900

    Revert "Remove lang selector message."
    This reverts commit 4cec1db3b39b5ff04f0796919022b8d2a2b0fcc5.

--- src/components/SelectLanguage.js
+++ src/components/SelectLanguage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
+import { FormattedMessage } from 'react-intl';
 
 const SelectLanguage = (props) => {
   const links = props.langs.map(lang =>
@@ -13,6 +14,9 @@ const SelectLanguage = (props) => {
 
   return (
     <section>
+      <header>
+        <FormattedMessage id="selectLanguage" />
+      </header>
       <ul>
         {links}
       </ul>
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,16 @@ import PropTypes from 'prop-types'
 import Header from './Header'
 import Helmet from 'react-helmet'
 import { getCurrentLangKey, getLangs, getUrlForLang } from 'ptz-i18n';
-import { IntlProvider } from 'react-intl';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import 'intl';
+
+import en from 'react-intl/locale-data/en';
+import 'intl/locale-data/jsonp/en';
+import ja from 'react-intl/locale-data/ja';
+import 'intl/locale-data/jsonp/ja';
+
+// add concatenated locale data
+addLocaleData([...en, ...ja]);
 
 class TemplateWrapper extends Component {
   constructor(props) {
@@ -18,11 +27,17 @@ class TemplateWrapper extends Component {
     this.langKey = getCurrentLangKey(langs, defaultLangKey, url);
     this.homeLink = `/${this.langKey}/`;
     this.langsMenu = getLangs(langs, this.langKey, getUrlForLang(this.homeLink, url));
+
+    // get the appropriate message file based on langKey
+    // at the moment this assumes that langKey will provide us
+    // with the appropriate language code
+    this.i18nMessages = require(`../data/messages/${this.langKey}`);
   }
   render() {
     return (
       <IntlProvider
         locale={this.langKey}
+        messages={this.i18nMessages}
       >
         <div>
           <Helmet
